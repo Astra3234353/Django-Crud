5c183e99048bc49f24337649f12d58b2772fbc8a
AstraDev <Xc0rpi0n145.com@gmail.com>
2025-07-16 15:28:59 -0600

    Signup form and db save with user cookie

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login
 
 # Create your views here.
+
+def home(request):
+  return render(request, 'home.html')
+
+def signup(request):
+  if request.method == 'GET':
+    return render(request, 'signup.html', {
+     'form': UserCreationForm
+    })
+  elif request.method == 'POST':
+    if request.POST['password1'] == request.POST['password2']:
+      try:
+        user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+        user.save()
+        login(request, user)
+        return  redirect('tasks')
+      except:
+        return render(request, 'signup.html', {
+            'form': UserCreationForm,
+            'error': 'Username already exists'
+        })
+    else:
+      return render(request, 'signup.html', {
+            'form': UserCreationForm,
+            'error': 'Passwords do not match'
+        })
+
+def tasks(request):
+  return render(request, 'tasks.html')
