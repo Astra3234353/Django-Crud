aa3af4dda9617be409e056e3fc95c0d76ddd637a
AstraDev <Xc0rpi0n145.com@gmail.com>
2025-07-17 11:49:31 -0600

    Finish authentication,login,logout and signin interface

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
 
@@ -31,5 +31,32 @@ def signup(request):
             'error': 'Passwords do not match'
         })
 
+def signout(request):
+  logout(request)
+  return redirect('home')
+
+def signin(request):
+  if request.method == 'GET':
+    return render(request, 'signin.html', {
+      'form': AuthenticationForm
+    })
+  elif request.method == 'POST':
+    user = authenticate(
+      request, username=request.POST['username'], 
+      password=request.POST['password']
+      )
+    
+    if user == None:
+      return render(request, 'signin.html', {
+        'form': AuthenticationForm,
+        'error': 'Username or password is incorrect'
+      })
+    else:
+      login(request, user)
+      return redirect('tasks')
+
+
+
 def tasks(request):
   return render(request, 'tasks.html')
+
